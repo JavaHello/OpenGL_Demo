8c23a2a45d11491770234ce657d3673128fcf01f
luokai <kaiv2@outlook.com>
2019-12-29 16:00:44 +0800

    相机类

---
+++ shader/demo_27/shader.fs
@@ -0,0 +1,16 @@
+#version 330 core
+
+out vec4 FragColor;
+
+in vec2 TexCoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+uniform float mixValue;
+uniform int dv;
+
+void main()
+{
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(abs(dv - TexCoord.x), TexCoord.y)), mixValue);
+}
