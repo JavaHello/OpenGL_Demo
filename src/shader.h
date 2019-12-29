f8c266d9705cfe69268757cd996fd35fa32bb9da
luokai <kaiv2@outlook.com>
2019-12-29 13:33:44 +0800

    进入3D

--- src/shader.h
+++ src/shader.h
@@ -1,7 +1,11 @@
 #ifndef SHADER_H
 #define SHADER_H
 
-#include <glad/glad.h>; // 包含glad来获取所有的必须OpenGL头文件
+// 包含glad来获取所有的必须OpenGL头文件
+#include <glad/glad.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include <string>
 #include <fstream>
@@ -22,6 +26,7 @@ public:
     void setBool(const std::string &name, bool value) const;
     void setInt(const std::string &name, int value) const;
     void setFloat(const std::string &name, float value) const;
+    void setMat4(const std::string &name, glm::mat4 value);
 };
 
 #endif
